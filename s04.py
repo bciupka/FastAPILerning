64f574a507e4678d1db80558d4806e53c5ce6f45
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-03-20 14:40:38 +0100

    header parameters

--- s04.py
+++ s04.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Cookie, Request
+from fastapi import FastAPI, Cookie, Request, Header
 from typing import Annotated
 
 app = FastAPI()
@@ -11,3 +11,11 @@ async def cookie_param_get(
     to_return = request.headers
     to_return_c = request.cookies
     return {"cookie": cookieparam, "headers_get": to_return, "cookie_get": to_return_c}
+
+
+@app.get("/headers/")
+async def header_param_get(
+    header: Annotated[str | None, Header()] = None,
+    header_2: Annotated[list[str] | None, Header()] = None,
+):
+    return {"header": header, "header_2": header_2}
