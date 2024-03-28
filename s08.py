25e69f415a3e26116b15f5da6169102f29b00a4b
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-03-28 14:51:36 +0100

    dependencies with yield

--- s08.py
+++ s08.py
@@ -3,7 +3,7 @@ from typing import Annotated
 
 
 async def dependency_3(token: str):
-    if token != "09871234":
+    if token != "123":
         raise HTTPException(status.HTTP_401_UNAUTHORIZED)
 
 
@@ -25,8 +25,39 @@ async def dependency_2(active: Annotated[bool, Header()]):
     raise HTTPException(status.HTTP_423_LOCKED, {"message": "not active!"})
 
 
+async def dependency_w_yield_1(a: int):
+    yield a
+    a = a + 2
+    print(f"New a: {a}")
+    # raise HTTPException(400, {"a": a})
+
+
+async def dep_w_yield_try(a: int, b: int):
+    try:
+        c = a / b
+        yield c
+    except ZeroDivisionError as e:
+        print(e)
+        raise HTTPException(400, str(e) + " DEP")
+    finally:
+        print(a, b)
+
+
 @app.get(
     "/dependencies_test", dependencies=[Depends(dependency_1), Depends(dependency_2)]
 )
 async def test():
     return "OK"
+
+
+@app.get("/dep_yield_test")
+async def yield_test(dep: Annotated[int, Depends(dependency_w_yield_1)]):
+    if dep > 5:
+        raise HTTPException(400, f"wrong a = {dep}")
+    return {"a": dep}
+
+
+@app.get("/dep_yield_try")
+async def try_test(dep: Annotated[int, Depends(dep_w_yield_try)]):
+    final = 10 / (dep - 2)
+    return final
