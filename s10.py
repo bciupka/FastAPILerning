c20725bf14bd6041c4223d667cc27e38438680c8
Bartlomiej Ciupka <bartek.ciupka@gmail.com>
2024-04-03 10:28:25 +0200

    CORS (cross-origin resource sharing)

--- s10.py
+++ s10.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
 import time
 
 app = FastAPI()
+
+origins = ["http://localhost", "http://localhost:8000"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_headers=["*"],
+)
 
 
 @app.middleware("http")
